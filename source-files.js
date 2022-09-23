a0f12c512fb2a61f5205f3d0897477e6bec936a4
realaravinth <realaravinth@users.noreply.github.com>
2022-09-23 09:38:50 +0000

    Deploying to gh-pages from  @ 803ce4470782d0bdc8f7b98663b39482c0233b63 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["dumbserve"] = {"name":"","dirs":[{"name":"api","dirs":[{"name":"v1","files":["files.rs","meta.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["ctx.rs","errors.rs","main.rs","routes.rs","settings.rs"]};
+var sourcesIndex = JSON.parse('{\
+"dumbserve":["",[["api",[["v1",[],["files.rs","meta.rs","mod.rs"]]],["mod.rs"]]],["ctx.rs","errors.rs","main.rs","routes.rs","settings.rs"]]\
+}');
 createSourceSidebar();
