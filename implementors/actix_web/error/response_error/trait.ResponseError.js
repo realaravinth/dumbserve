c4ba2fba72fd062eeda9e516a6104734a6a0db12
realaravinth <realaravinth@users.noreply.github.com>
2022-11-04 09:34:18 +0000

    Deploying to gh-pages from  @ 803ce4470782d0bdc8f7b98663b39482c0233b63 🚀

--- implementors/actix_web/error/response_error/trait.ResponseError.js
+++ implementors/actix_web/error/response_error/trait.ResponseError.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["dumbserve"] = [{"text":"impl ResponseError for <a class=\"enum\" href=\"dumbserve/errors/enum.ServiceError.html\" title=\"enum dumbserve::errors::ServiceError\">ServiceError</a>","synthetic":false,"types":["dumbserve::errors::ServiceError"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"dumbserve":[["impl ResponseError for <a class=\"enum\" href=\"dumbserve/errors/enum.ServiceError.html\" title=\"enum dumbserve::errors::ServiceError\">ServiceError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/lazy_static/trait.LazyStatic.js
+++ implementors/lazy_static/trait.LazyStatic.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["dumbserve"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/lazy_static/1.4.0/lazy_static/trait.LazyStatic.html\" title=\"trait lazy_static::LazyStatic\">LazyStatic</a> for <a class=\"struct\" href=\"dumbserve/struct.SETTINGS.html\" title=\"struct dumbserve::SETTINGS\">SETTINGS</a>","synthetic":false,"types":["dumbserve::SETTINGS"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"dumbserve":[["impl <a class=\"trait\" href=\"https://docs.rs/lazy_static/1.4.0/lazy_static/trait.LazyStatic.html\" title=\"trait lazy_static::LazyStatic\">LazyStatic</a> for <a class=\"struct\" href=\"dumbserve/struct.SETTINGS.html\" title=\"struct dumbserve::SETTINGS\">SETTINGS</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
