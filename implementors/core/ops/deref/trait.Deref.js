e0f4f6af4eab19b38217a22201356b2554eef96c
realaravinth <realaravinth@users.noreply.github.com>
2023-03-29 09:37:59 +0000

    Deploying to gh-pages from  @ db6d4a34b9716ad0115d298ea24127f268c2b818 🚀

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"dumbserve":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"dumbserve/struct.SETTINGS.html\" title=\"struct dumbserve::SETTINGS\">SETTINGS</a>"]]
+"dumbserve":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"dumbserve/struct.SETTINGS.html\" title=\"struct dumbserve::SETTINGS\">SETTINGS</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
